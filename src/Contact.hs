bced0d18e8cf1eef814f62d6a4c0ce97818e87e8
rashadg1030 <rashad.sasaki@gmail.com>
2019-04-20 00:37:34 -0700

    Made some components.

--- src/Contact.hs
+++ src/Contact.hs
@@ -9,37 +9,48 @@ import Lucid
 import Lucid.Html5
 import Lucid.Base
 
-test :: Html ()
-test = p_ [class_ "blue"] "123 < 456"
-
 mkListItem :: Html () -> Text -> Html ()
 mkListItem t link = li_ (a_ [href_ link] t)
 
-listItems :: [Html ()]
-listItems = [ mkListItem "Home" "#index"
-            , mkListItem "Blog" "#blog"
-            , mkListItem "Laboratory" "#laboratory"
-            , mkListItem "Art" "#art"
-            , mkListItem "Contact" "#contact" 
-            ]
+navBar :: Html ()
+navBar = ul_ $ mconcat [ mkListItem "Home" "#index"
+                       , mkListItem "Blog" "#blog"
+                       , mkListItem "Laboratory" "#laboratory"
+                       , mkListItem "Art" "#art"
+                       , mkListItem "Contact" "#contact" 
+                       ]
 
 resumeButton :: Html ()
-resumeButton = button_ resume
+resumeButton = button_ resume 
 
 resume :: Html ()
 resume = a_ [href_ "./Rashad_Gover.pdf", download_ "Rashad_Gover"] "Download Resume"
 
+header :: Html ()
+header = head_ ( do meta_ [charset_ "utf-8"]
+                    meta_ [ name_ "viewport"
+                        , content_ "width=device-width, initial-scale=1"
+                        ]
+                    link_ [ href_ "./style.css"
+                        , rel_ "stylesheet"
+                        , type_ "text/css"
+                        ]
+                    link_ [ href_ "//fonts.googleapis.com/css?family=Open+Sans" 
+                        , rel_ "stylesheet"
+                        , type_ "text/css"
+                        ]
+                    link_ [ href_ "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.0/css/bootstrap.min.css"
+                        , rel_ "stylesheet"
+                        , type_ "text/css"]
+                    title_ "rλshλd1030" )
 -- Add Canon Later
 
 template :: Html () -> Html ()
-template inner = do
-                  h1_ "rλshλd1030"
-                  inner
-                  p_ "This is my first entry." 
-                  ul_ (mconcat listItems)  
-                  resumeButton
+template inner = doctypehtml_ ( do header
+                                   body_ ( do h1_ "rλshλd1030"
+                                              inner
+                                              p_ "This is my first entry." 
+                                              navBar  
+                                              resumeButton ))
               
-
-test2 :: Html () 
-test2 = table_ (tr_ (td_ (p_ "Hello, World!")))
 
