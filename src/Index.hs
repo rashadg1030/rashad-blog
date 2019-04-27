d21f2542ae1981be97ce2690551b301a06926b08
rashadg1030 <rashad.sasaki@gmail.com>
2019-04-26 21:32:54 -0700

    Added contact form.

--- src/Index.hs
+++ src/Index.hs
@@ -14,13 +14,22 @@ import Lucid.Base
 indexPage :: Html ()
 indexPage = baseTemplate (do splashContainer "Rashad Gover" "Abstraction Engineer for Hire" "Download Resume" "./Rashad_Gover.pdf" -- Need content container after this!!
                              contentWrapper $ (do content "What I Do" $ grid3col (div1 "Learn" randomText) (div1 "Build" randomText) (div1 "Teach" randomText)
-                                                  ribbon (img_ [class_ "pure-img-responsive", src_ "../img/education-graphing-paper-homework-167682.jpg", width_ "300"]) (do h2_ [class_ "content-head content-head-ribbon"] "Laboris nisi ut aliquip."; p_ "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                                                  with (content "Good Vibrations" $ gridAsymm testDiv testDiv) [class_ " content-bottom"]
+                                                  ribbon (img_ [class_ "pure-img-responsive", src_ "../img/education-graphing-paper-homework-167682.jpg", width_ "300"]) (do h2_ [class_ "content-head content-head-ribbon"] "Good Vibrations"; p_ randomText)
+                                                  with (content "Contact Me" $ gridAsymm contactForm testDiv) [class_ " content-bottom"]
                                                   footer $ p_ "This the footer"))
 
 div1 :: Html () -> Html () -> Html ()
 div1 title para = (do h1_ title 
                       p_  para)
+
+contactForm :: Html ()
+contactForm = form_ [class_ "pure-form pure-form-stacked"] (fieldset_ (do label_ [for_ "name"] "Your Name"
+                                                                          input_ [id_ "name", type_ "text", placeholder_ "Your Name"]
+                                                                          label_ [for_ "email"] "Your Email"
+                                                                          input_ [id_ "email", type_ "email", placeholder_ "Your Email"]
+                                                                          label_ [for_ "message"] "Your Message"
+                                                                          textarea_ [id_ "message", placeholder_ "Your Message"] ""
+                                                                          div_ [class_ "is-center"] $ button_ [class_ "pure-button", type_ "submit"] "Send"))
 
 randomText :: Html ()
 randomText = "Lorem ipsum do lasd did ads. do re mi fas goj dhasd yt aohdfj deeyu iope. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
