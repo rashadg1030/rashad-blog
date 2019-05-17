453c9bc39bab0eae8eda999c12dd9465e9f7edce
rashadg1030 <rashad.sasaki@gmail.com>
2019-05-17 13:09:13 -0700

    Simplified CSS.

--- src/Pages/Contact.hs
+++ src/Pages/Contact.hs
@@ -26,21 +26,3 @@ contactPage = pre_ [class_ "contact-info"] ( do "contactInfo :: Rashad"
                                                 (a_ [href_ "https://www.linkedin.com/in/rashad-gover-2557b1132/"] "\"https://www.linkedin.com/in/rashad-gover-2557b1132/\"")
                                                 " }"
                                             ) 
-
--- CONTACT PAGE DESIGN --
-{--
-<pre><code>contactInfo :: Rashad 
-<span>contactInfo</span> = Contact { <span>email</span>     = <span>"rashad.sasaki@gmail.com"</span>
-                      , <span>location</span>  = <span>"Los Angeles, CA"</span>
-                      , <span>linkedIn</span>  = <span>""</span>
-                      , <span>instagram</span> = <span>"COMING SOON"</span> }
-</code></pre>
---}
-
-{--
-contactInfo :: Rashad 
-contactInfo = Contact { email     = "rashad.sasaki@gmail.com"
-                      , location  = "Los Angeles, CA"
-                      , linkedIn  = ""
-                      , instagram = "COMING SOON" } 
---}
